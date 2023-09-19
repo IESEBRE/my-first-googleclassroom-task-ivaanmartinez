dc53bf452bf0fbfde06e8cae3ecdcd27e7645297
Ivan Martinez <ivanmartinez1@iesebre.com>
2023-09-19 13:14:56 +0200

    He afegit el primer  projecte Java del curs

---
+++ PrimerProjecte/src/main/java/org/example/Main.java
@@ -0,0 +1,13 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.println("Segona liena de text");
+        System.out.print("Me dic Ivan ");
+        System.out.println("i soc un alumne");
+    }
+
+
+}
+
